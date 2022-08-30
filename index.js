a59365eaef1b08a2a4e2024cbae64717eb5c15d0
Arjen Brouwer <arjen@ag5.com>
2022-08-30 09:55:37 +0200

    Added debug output

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const {parse} = require('./terraform')
 
 try {
     const plan = core.getInput('plan');
+    console.debug("Plan input: ", plan)
 
     const {
         status, hasChanges, mutations, report
     } = parse(plan)
+
+    console.debug("Status: ", status)
+    console.debug("Changes: ", hasChanges)
+    console.debug("Mutations: ", mutations)
 
     core.setOutput("status", status);
     core.setOutput("hasChanges", hasChanges);
